3ed2a6c4e857835e39aaaed3a640b33ba7550b1b
Josh Frankel <joshmfrankel@gmail.com>
2013-05-15 23:26:18 -0500

    Basic OOP plugin structure

--- js/socializr.js
+++ js/socializr.js
@@ -19,9 +19,53 @@
  */
 (function($) {
 
+    // The Socializr Class Definition
+    var Socializr = function(element, options) {
+
+        // Store the basic class data
+        var elem = $(element);
+        var obj = this;
+
+        // Default Plugin properties
+        var defaults = {
+            services: []
+        };
+
+        // Merge the passed in options with the defaults
+        options = $.extend({}, defaults, options);
+
+        // Public method
+        this.publicMethod = function() {
+            console.log('publicMethod() called!');
+            console.log(options);
+            console.log(elem);
+        };
+
+        // Private method
+        var privateMethod = function() {
+            console.log('private called');
+        };
+    };
+
     // Register the plugin name as a function in jQuery
     // Takes options parameter for overridding defaults
     $.fn.socializr = function(options) {
+
+        // Allow chaining by returning the this keyword
+        return this.each(function() {
+
+            // Cache the jquery keyword for future usage
+            var $this = $(this);
+
+            // If the obj alread exists return
+            if ($this.data('socializr')) return;
+
+            // Instantiate a new Socializr Obj
+            var socializr = new Socializr(this, options);
+
+            // Store plugin object in this element's data
+            $this.data('socializr', socializr);
+        });
 
     }; // End Plugin
 
