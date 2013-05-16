783cf7e25e15304d2b3a5bad8bd9460f0117a585
Josh Frankel <joshmfrankel@gmail.com>
2013-05-15 22:28:24 -0500

    Initial file docblock

---
+++ js/socializr.js
@@ -0,0 +1,13 @@
+/**
+ * Socializr
+ *
+ * A Social Feed widget for multiple networks.  Most notably is support
+ * for twitter 1.1.
+ *
+ * @author Joshua Frankel <joshmfrankel@gmail.com>
+ * @copyright Copyright (c) 2013, Joshua Frankel
+ * @link http://www.joshfrankel.me
+ * @link https://github.com/joshmfrankel/socializr
+ * @see  Twitteroauth https://github.com/abraham/twitteroauth for authentication details
+ * @version 0.1
+ */
